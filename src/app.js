1b238523acbd07f1bbeafbeacfc6f79e6f731d6f
Mohammed Ali <mohammed.ali.baaqail@gmail.com>
2024-03-03 16:54:55 +0300

    ..

--- src/app.js
+++ src/app.js
@@ -5,43 +5,41 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const middlewares = require('./middlewares');
 const commentsRoutes = require('./api/comments');
-// const gamesRoutes = require('./routes/games');
-const userRoutes = require('./api/user')
+const userRoutes = require('./api/user');
 
 const port = process.env.PORT || 3000;
-// Create express app
+
 const app = express();
-app.use((req, res, next) => {
-    res.setHeader('Permissions-Policy', 'interest-cohort=(), attribution-reporting=*, run-ad-auction=*, join-ad-interest-group=*, browsing-topics=*');
-    next();
-  });
-  const corsOptions = {
+
+const corsOptions = {
     origin: '*',
 };
 
-app.use(cors(corsOptions));
-//middleware
+app.use(cors(corsOptions)); // Applying cors middleware first
+
+app.use((req, res, next) => {
+    res.setHeader('Permissions-Policy', 'interest-cohort=(), attribution-reporting=*, run-ad-auction=*, join-ad-interest-group=*, browsing-topics=*');
+    next();
+});
+
 app.use(express.json());
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Internal Server Error');
-  });
-//routes
-app.use('/game/comments' ,commentsRoutes);
-// app.use('/games' ,gamesRoutes);
+});
+
+app.use('/game/comments', commentsRoutes);
 app.use('/user', userRoutes);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
-// Connect to MongoDB
+
 const connectionString = process.env.DATABASE_URL;
 mongoose.set('strictQuery', false);
-mongoose.connect(connectionString)
-    // .then(() => {
-    //     app.listen(port, () => {
-    //         console.log('connected to db and listening on port ' , port );
-    //     })
-    // })
-    // .catch(err => console.log(err));
+mongoose.connect(connectionString);
+
+app.listen(port, () => {
+    console.log('Connected to db and listening on port', port);
+});
 
 module.exports = app;
